7492779f26c4fe116fae5d29d07c8752ccd98fe9
Joyce Hong <joyce.hong8@gmail.com>
2025-11-17 12:08:10 -0800

    add simple setup option

--- push.sh
+++ push.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+Help() 
+{
+	echo
+	echo "Copy the configuration files in this repository into their functional locations"
+	echo
+	echo "Syntax: push [-s|h]"
+	echo "options:"
+	echo "s    Simple setup (push vanilla vim and tmux configurations only)"
+	echo "h    Print this help page"
+	echo
+}
+
+while getopts "hs" option; do
+	case $option in
+		h) # display Help
+			Help
+			exit;;
+		s) # Simple setup
+			run_simple=true;;
+		\?) # Invalid option
+			echo "Error: Invalid option"
+			exit;;
+	esac
+done
+
 cd linux
 cp -ri -t ~/ .tmux.conf .vimrc
 
-cd .config
-mkdir ~/.config
-cp -ri -t ~/.config/ tmux-powerline nvim
+if [ ! $run_simple ]; then
+	cd .config
+	mkdir ~/.config
+	cp -ri -t ~/.config/ tmux-powerline nvim
+fi
+
