6e1a8f8527bcdbb058807428ccca68b522ca5f03
Joyce Hong <joyce.hong8@gmail.com>
2026-01-08 12:08:11 -0800

    alt tmux message for small width

--- linux/.tmux/welcome.sh
+++ linux/.tmux/welcome.sh
@@ -9,8 +9,12 @@ NC='\033[0m' # No Color
 C_LOGO_FRONT=$GREEN
 C_LOGO_BACK=$GRAY
 
+WIDTH=$(tput cols)
+
+INTRO_TEXT="WELCOME TO"
+
 # ascii font: ogre
-tmux_ascii='
+tmux_logo='
                               /\                             
                            <_//\\_>                          
                       ______//  \\______                     
@@ -23,6 +27,19 @@ tmux_ascii='
                      //       ||        \\                   
                     /         ||          \                  
 '
+
+logo_mini='
+ _____           __  __
+/__   \/\/\  /\ /\ \/ /
+  / /\/    \/ / \ \  / 
+ / / / /\/\ \ \_/ /  \ 
+ \/  \/    \/\___/_/\_\
+'
+
+# TODO: Logo width is 61...make a more round number later
+# (will have to change color offsets)
+LOGO_WIDTH=$(printf "$tmux_logo" | head -2 | wc -m)
+LOGO_WIDTH=$((LOGO_WIDTH - 2))
 
 # TODO: automatically deal with color code offsets so positions don't change
 # Can use ansi 255 colors after this
@@ -51,29 +68,39 @@ for conf in "${color_changes[@]}"; do
 	p="${conf% *}"
 	offset="${conf#* }"
 	end=$((p+offset))
-	tmux_ascii=$(printf "%s${C_LOGO_FRONT}%s${C_LOGO_BACK}%s" \
-						 "${tmux_ascii:0:p}" "${tmux_ascii:p:offset}" "${tmux_ascii:end}")
+	tmux_logo=$(printf "%s${C_LOGO_FRONT}%s${C_LOGO_BACK}%s" \
+						 "${tmux_logo:0:p}" "${tmux_logo:p:offset}" "${tmux_logo:end}")
 done
-
-width=$(tput cols)
 
 center() {
 	# Don't count color codes in length
 	len=$(echo "$1" | sed "s/$(echo -e "\e")[^m]*m//g");
-	printf "%$(((width-${#len})/2))s%s\n" "" "$1"
+	printf "%$(((WIDTH-${#len})/2))s%s\n" "" "$1"
 }
 
+if [ "$WIDTH" -lt "$LOGO_WIDTH" ]; then
+	mini_logo_width=$(printf "$logo_mini" | head -2 | wc -m)
+	mini_logo_width=$(( mini_logo_width - 2 ))
+	spacing=$(( mini_logo_width / 2 + 5 ))	
+
+	printf "%${spacing}s%s" "${INTRO_TEXT}"
+  printf "${C_LOGO_FRONT}"
+	printf "$logo_mini"
+	echo
+	exit
+fi
+
 echo
-center "WELCOME TO"
+center "${INTRO_TEXT}"
 
 printf "${C_LOGO_BACK}"
-echo "$tmux_ascii" | while IFS= read -r line; do
+echo "$tmux_logo" | while IFS= read -r line; do
 	center "$line"
 done
 printf "${NC}"
 echo
 
-max_line_len=60
+max_line_len=$(( LOGO_WIDTH - 2 ))
 
 # get quotes from square brackets
 # randomly pick one
